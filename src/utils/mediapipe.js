9fcbbd25d1c5135a7345e9366c01fb610b1cb054
Renan Tashiro <renantashiro@hotmail.com>
2023-04-17 15:18:41 -0300

    refactor(): extract mediapipe utils

---
+++ src/utils/mediapipe.js
@@ -0,0 +1,36 @@
+import { Hands } from "@mediapipe/hands";
+import { Pose } from "@mediapipe/pose";
+
+export function initalizeHandsDetector() {
+  const hands = new Hands({
+    locateFile: (file) => {
+      return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+    },
+  });
+
+  hands.setOptions({
+    maxNumHands: 2,
+    modelComplexity: 1,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5,
+  });
+
+  return hands;
+}
+
+export function initializePoseDetector() {
+  const pose = new Pose({
+    locateFile: (file) => {
+      return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+    },
+  });
+
+  pose.setOptions({
+    modelComplexity: 1,
+    smoothLandmarks: false,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5,
+  });
+
+  return pose;
+}
