c68556ed6f10cc601cb25bf6842f2217d3bf46c6
Renan Tashiro <renantashiro@hotmail.com>
2023-04-27 13:11:29 -0300

    feat(): images cm mapper

--- src/signs/signs.js
+++ src/signs/signs.js
@@ -1,3 +1,102 @@
+export const handshapeImages = {
+  a_cm: [
+    {
+      label: "Visão frontal",
+      path: "/handshapes/a_front.png",
+    },
+    {
+      label: "Visão lateral",
+      path: "/handshapes/a_side.png",
+    },
+  ],
+  index_finger_cm: [
+    {
+      label: "Visão frontal",
+      path: "/handshapes/index_finger_front.png",
+    },
+    {
+      label: "Visão lateral",
+      path: "/handshapes/index_finger_side.png",
+    },
+  ],
+  l_cm: [
+    {
+      label: "Visão frontal",
+      path: "/handshapes/l_front.png",
+    },
+    {
+      label: "Visão lateral",
+      path: "/handshapes/l_side.png",
+    },
+  ],
+  middle_index_finger_cm: [
+    {
+      label: "Visão frontal",
+      path: "/handshapes/middle_index_finger_front.png",
+    },
+    {
+      label: "Visão lateral",
+      path: "/handshapes/middle_index_finger_side.png",
+    },
+  ],
+  o_cm: [
+    {
+      label: "Visão frontal",
+      path: "/handshapes/o_front.png",
+    },
+    {
+      label: "Visão lateral",
+      path: "/handshapes/o_side.png",
+    },
+  ],
+  oi_cm: [
+    {
+      label: "Visão frontal",
+      path: "/handshapes/oi_front.png",
+    },
+    {
+      label: "Visão lateral",
+      path: "/handshapes/oi_side.png",
+    },
+  ],
+  open_hand_fingers_apart_cm: [
+    {
+      label: "Visão frontal",
+      path: "/handshapes/open_hand_fingers_apart_front.png",
+    },
+    {
+      label: "Visão lateral",
+      path: "/handshapes/open_hand_fingers_apart_side.png",
+    },
+  ],
+  s_cm: [
+    {
+      label: "Visão frontal",
+      path: "/handshapes/s_front.png",
+    },
+    {
+      label: "Visão lateral",
+      path: "/handshapes/s_side.png",
+    },
+  ],
+  thumbs_up_cm: [
+    {
+      label: "Visão frontal",
+      path: "/handshapes/thumbs_up_front.png",
+    },
+  ],
+  y_cm: [
+    {
+      label: "Visão frontal",
+      path: "/handshapes/y_front.png",
+    },
+    {
+      label: "Visão lateral",
+      path: "/handshapes/y_side.png",
+    },
+  ],
+};
+
 export const signs = [
   {
     language: "libras",
@@ -37,7 +136,7 @@ export const signs = [
       startPosition: {
         dominantHand: {
           bodyRegion: "chin",
-          handConfiguration: "s_cm",
+          handConfiguration: "o_cm",
           palmDirection: { z: -1, x: 0, y: 0 },
         },
         nonDominantHand: {},
