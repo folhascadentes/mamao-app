897a1fb059b63ec8ec19993b444b354fcffa74f0
Renan Tashiro <renantashiro@hotmail.com>
2023-04-28 20:19:23 -0300

    feat(): increase & decrease font

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import papayaLogo from "./assets/papaya.png";
 
 function Header() {
+  const [fontSize, setFontSize] = useState(100);
+
+  const handleIncreaseFontSize = () => {
+    setFontSize(fontSize + 10);
+  };
+
+  const handleDecreaseFontSize = () => {
+    setFontSize(fontSize - 10);
+  };
+
   return (
     <header className="w-full">
+      <style>{`html {font-size: ${fontSize}%; }`}</style>
       <div className="container mx-auto px-4 py-4 flex space-x-4 justify-between items-center">
+        <div className="w-44"></div>
         <div className="container mx-auto px-4 py-4 flex justify-center items-center">
-          <img src={papayaLogo} alt="Papaya Logo" className="h-20" />
+          <img src={papayaLogo} alt="Papaya Logo" style={{ height: "80px" }} />
         </div>
+        <button
+          title="Ação de aumentar tamanho do texto [+]"
+          aria-describedby="Ação de aumentar tamanho do texto"
+          className={`hover:bg-gray-200 rounded-xl p-4 font-bold`}
+          onClick={handleIncreaseFontSize}
+          style={{ fontSize: "24px" }}
+        >
+          A+
+        </button>
+        <button
+          title="Ação de aumentar diminuir do texto [-]"
+          aria-describedby="Ação de aumentar diminuir do texto"
+          className={`hover:bg-gray-200 rounded-xl p-4 font-bold`}
+          onClick={handleDecreaseFontSize}
+          style={{ fontSize: "24px" }}
+        >
+          A-
+        </button>
       </div>
     </header>
   );
