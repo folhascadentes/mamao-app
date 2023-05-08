3adbc243d0f9f871a81eca8e8dc41cf057bf2839
Renan Tashiro <renantashiro@hotmail.com>
2023-05-08 19:33:52 -0300

    feat(): dependencies loading time

--- src/LoadingScreen.js
+++ src/LoadingScreen.js
@@ -1,19 +1,104 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+const ProgressBar = ({ url, progress, contentLength, elapsedTime }) => {
+  const remainingTime = (elapsedTime / progress) * (100 - progress);
+  const estimatedTime = remainingTime;
+
+  return (
+    <div className="flex flex-col space-y-2">
+      <div
+        style={{
+          width: "100%",
+          backgroundColor: "#eee",
+          borderRadius: "0.25rem",
+        }}
+      >
+        <div
+          style={{
+            width: `${progress}%`,
+            backgroundColor: "#5EA8FD",
+            height: "16px",
+            transition: "width 0.5s",
+            borderRadius: "0.25rem",
+          }}
+        ></div>
+      </div>
+      <div className="flex">
+        <div>{progress.toFixed(2)}%</div>
+        <div className="flex-grow"></div>
+        <div>{estimatedTime.toFixed(2)} segundos</div>
+      </div>
+      <div className="flex flex-col text-sm">
+        <div>Dependencia: {url}</div>
+        <div>Tamanho do Conteúdo: {contentLength} bytes</div>
+      </div>
+    </div>
+  );
+};
 function LoadingScreen() {
+  const [progressData, setProgressData] = useState({});
+  const [counter, setCounter] = useState(0);
+  let patchApplied = false;
+
+  const patchXMLHttpRequest = () => {
+    if (patchApplied) {
+      return;
+    }
+
+    patchApplied = true;
+
+    const originalOpen = XMLHttpRequest.prototype.open;
+
+    XMLHttpRequest.prototype.open = function (...args) {
+      const url = args[1];
+      const startTime = new Date();
+
+      setCounter((counter) => counter + 1);
+
+      this.addEventListener("progress", (event) => {
+        if (event.lengthComputable) {
+          const percentComplete = (event.loaded / event.total) * 100;
+          const elapsedTime = (new Date() - startTime) / 1000;
+          setProgressData({
+            url,
+            progress: percentComplete,
+            contentLength: event.total,
+            elapsedTime,
+          });
+        } else {
+          setProgressData({ url, progress: 0 });
+        }
+      });
+
+      originalOpen.apply(this, args);
+    };
+  };
+
+  useEffect(() => {
+    patchXMLHttpRequest();
+  }, []);
+
   return (
     <div id="loading-screen" className="text-white px-4">
-      <div className="flex flex-col space-y-4 items-center justify-center">
-        <div className="spinner mb-8"></div>
-        <div className="text-3xl">
-          Configurando sistema de captura
-          <br />
-          Aguarde cerca de 10 segundos
-        </div>
-        <div>
-          Estamos carregando o sistema de inteligência artificial para
-          reconhecer seus movimentos
-        </div>
+      <div className="flex flex-col space-y-8 items-center justify-center">
+        <div className="text-3xl">Configurando sistema de captura</div>
+        {!progressData.url && <div className="spinner"></div>}
+        {progressData.url && (
+          <div className="flex-col space-y-4">
+            <div className="text-lg">
+              Estamos carregando o sistema de inteligência artificial para
+              reconhecer seus movimentos ({counter}/2)
+            </div>
+            <div className="w-100">
+              <ProgressBar
+                url={progressData.url}
+                progress={progressData.progress}
+                contentLength={progressData.contentLength}
+                elapsedTime={progressData.elapsedTime}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
