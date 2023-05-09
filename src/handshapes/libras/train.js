8d40cac8dcaec37448fa8802f999a938341b4ae8
Renan Tashiro <renantashiro@hotmail.com>
2023-05-09 19:12:15 -0300

    feat(): d

--- src/handshapes/libras/train.js
+++ src/handshapes/libras/train.js
@@ -80,6 +80,7 @@ const thumbFinger = require("./thumb_finger.js");
 const openHandThumbApart = require("./open_hand_thumb_apart.js");
 const handCupping = require("./hand_cupping.js");
 const shapeCFinger = require("./c.js");
+const shapeDFinger = require("./d.js");
 
 const inputData = [
   ...indexFingerData,
@@ -94,21 +95,25 @@ const inputData = [
   ...openHandThumbApart,
   ...handCupping,
   ...shapeCFinger,
+  ...shapeDFinger,
 ];
 
 const outputData = [
-  ...indexFingerData.map(() => [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-  ...middleAndIndexFingerData.map(() => [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-  ...oiCM.map(() => [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
-  ...openHand.map(() => [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]),
-  ...openHandFingersApart.map(() => [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]),
-  ...shapeAFinger.map(() => [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]),
-  ...shapeLFinger.map(() => [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]),
-  ...shapeSFinger.map(() => [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]),
-  ...thumbFinger.map(() => [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]),
-  ...openHandThumbApart.map(() => [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]),
-  ...handCupping.map(() => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]),
-  ...shapeCFinger.map(() => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
+  ...indexFingerData.map(() => [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+  ...middleAndIndexFingerData.map(() => [
+    0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]),
+  ...oiCM.map(() => [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+  ...openHand.map(() => [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+  ...openHandFingersApart.map(() => [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]),
+  ...shapeAFinger.map(() => [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]),
+  ...shapeLFinger.map(() => [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]),
+  ...shapeSFinger.map(() => [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]),
+  ...thumbFinger.map(() => [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]),
+  ...openHandThumbApart.map(() => [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]),
+  ...handCupping.map(() => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]),
+  ...shapeCFinger.map(() => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]),
+  ...shapeDFinger.map(() => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
 ];
 
 const { inputTrain, outputTrain, inputValidation, outputValidation } =
