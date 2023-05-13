edc5bc309227188300f7dbde0fa60589d1341bd0
Renan Tashiro <renantashiro@hotmail.com>
2023-05-13 19:29:43 -0300

    feat(): add offset to position via param

--- src/utils/positions.js
+++ src/utils/positions.js
@@ -6,68 +6,82 @@ export function getBodyRegionCoordinates(
   rightDominantHand = true
 ) {
   if (type === "chin") {
-    return getMiddlePoint(
+    return getMiddlePoint([
       landmarks[4],
       landmarks[1],
       landmarks[11],
-      landmarks[12]
-    );
+      landmarks[12],
+    ]);
   } else if (type === "torax") {
-    return getMiddlePoint(
+    return getMiddlePoint([
       landmarks[9],
       landmarks[10],
       landmarks[11],
       landmarks[12],
       landmarks[24],
-      landmarks[23]
-    );
+      landmarks[23],
+    ]);
   } else if (type === "torax_upper") {
-    return getMiddlePoint(
-      getMiddlePoint(landmarks[24], landmarks[23], landmarks[4], landmarks[1]),
+    return getMiddlePoint([
+      getMiddlePoint([
+        landmarks[24],
+        landmarks[23],
+        landmarks[4],
+        landmarks[1],
+      ]),
       landmarks[12],
-      landmarks[11]
-    );
+      landmarks[11],
+    ]);
   } else if (type === "torax_lower") {
-    return getMiddlePoint(
-      getMiddlePoint(landmarks[24], landmarks[23], landmarks[4], landmarks[1]),
+    return getMiddlePoint([
+      getMiddlePoint([
+        landmarks[24],
+        landmarks[23],
+        landmarks[4],
+        landmarks[1],
+      ]),
       landmarks[0],
       landmarks[24],
-      landmarks[23]
-    );
+      landmarks[23],
+    ]);
   } else if (type === "torax_left") {
-    return getMiddlePoint(landmarks[11], landmarks[23], landmarks[9]);
+    return getMiddlePoint([landmarks[11], landmarks[23], landmarks[9]]);
   } else if (type === "torax_right") {
-    return getMiddlePoint(landmarks[12], landmarks[24], landmarks[10]);
+    return getMiddlePoint([landmarks[12], landmarks[24], landmarks[10]]);
   } else if (type === "torax_right_lower") {
-    return getMiddlePoint(
-      getMiddlePoint(landmarks[24], landmarks[4], landmarks[1]),
-      landmarks[24]
-    );
+    return getMiddlePoint([
+      getMiddlePoint([landmarks[24], landmarks[4], landmarks[1]]),
+      landmarks[24],
+    ]);
   } else if (type === "arm_upper") {
     return rightDominantHand
-      ? getMiddlePoint(landmarks[13], {
-          x: landmarks[13].x,
-          y: landmarks[13].y - 200,
+      ? getMiddlePoint([landmarks[13]], {
+          x: 0,
+          y: -200,
         })
-      : getMiddlePoint(landmarks[14]);
+      : getMiddlePoint([landmarks[14]]);
   } else if (type === "arm_extended") {
     return rightDominantHand
-      ? getMiddlePoint(landmarks[11], landmarks[23])
-      : getMiddlePoint(landmarks[12], landmarks[24]);
+      ? getMiddlePoint([landmarks[11], landmarks[23]])
+      : getMiddlePoint([landmarks[12], landmarks[24]]);
   } else if (type === "wrist") {
     return rightDominantHand
-      ? getMiddlePoint(landmarks[15], {
-          x: landmarks[15].x,
-          y: landmarks[15].y - 100,
+      ? getMiddlePoint([landmarks[19]], {
+          x: 0,
+          y: -100,
         })
-      : getMiddlePoint(landmarks[16], {
-          x: landmarks[16].x,
-          y: landmarks[16].y - 100,
+      : getMiddlePoint([landmarks[20]], {
+          x: 0,
+          y: -100,
         });
+  } else if (type === "shoulder_left") {
+    return getMiddlePoint([landmarks[11]]);
+  } else if (type === "elbow_left") {
+    return getMiddlePoint([landmarks[13]]);
   }
 }
 
-export function getMiddlePoint(...coords) {
+export function getMiddlePoint(coords, offset = { x: 0, y: 0 }) {
   let totalX = 0;
   let totalY = 0;
   const len = coords.length;
@@ -80,5 +94,5 @@ export function getMiddlePoint(...coords) {
   const midX = totalX / len;
   const midY = totalY / len;
 
-  return { x: midX, y: midY };
+  return { x: midX + offset.x, y: midY + offset.y };
 }
