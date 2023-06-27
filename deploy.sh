b70ff09572c9e9d883934678899e21c54b182eca
Renan Tashiro <renantashiro@hotmail.com>
2023-06-27 16:34:56 -0300

    config():  aws region via env var

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,14 @@
 
 source .env
 
-# define as variáveis com as informações da sua aplicação
+npm run build
+
+REGION=$AWS_REGION
 S3_BUCKET_NAME=$S3_BUCKET_NAME
 CLOUDFRONT_DISTRIBUTION_ID=$CLOUDFRONT_DISTRIBUTION_ID
 
-# faz o build da aplicação
-npm run build
-
-# sincroniza os arquivos do build com o S3
 aws s3 sync build/ s3://$S3_BUCKET_NAME --delete
-
-# invalida o cache do CloudFront
+aws cloudfront update-distribution --id $CLOUDFRONT_DISTRIBUTION
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
 
 echo "Deploy concluído com sucesso!"
