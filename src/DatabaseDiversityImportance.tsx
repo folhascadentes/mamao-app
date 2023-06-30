44bbc1e42b93e773842bab34b116ac57e2abb448
RenanTashiro <renantashiro@hotmail.com>
2023-06-30 17:25:29 -0300

    fix(): improve spacing for low res screen for ddi

--- src/DatabaseDiversityImportance.tsx
+++ src/DatabaseDiversityImportance.tsx
@@ -3,10 +3,10 @@ import diversity from "./assets/diversity.png";
 
 function DatabaseDiversityImportance(): JSX.Element {
   return (
-    <div className="flex flex-wrap justify-center space-x-8 xl:space-x-24 mt-8 xl:mt-16 mb-24">
+    <div className="flex flex-wrap justify-center space-x-8 xl:space-x-16 mt-8 xl:mt-16 mb-24">
       <div
         className="flex flex-col space-y-4"
-        style={{ width: window.innerWidth <= 500 ? "auto" : "720px" }}
+        style={{ width: window.innerWidth <= 500 ? "auto" : "560px" }}
       >
         <h1 className="text-3xl md:text-4xl font-black mb-2">
           Por que precisamos de diferentes tipos de dados para fazer um bom
