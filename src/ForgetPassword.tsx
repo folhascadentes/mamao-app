b69d628a702a1dc122082914f6649b0557bbd76a
Renan Tashiro <renantashiro@hotmail.com>
2023-06-30 21:00:12 -0300

    fix(): mobile for ForgetPassword

--- src/ForgetPassword.tsx
+++ src/ForgetPassword.tsx
@@ -60,10 +60,10 @@ function ForgetPassword(): JSX.Element {
   return (
     <div className="flex flex-wrap justify-center space-x-24 mt-8 xl:mt-16 mb-24">
       <div
-        className="flex flex-col justify-center space-y-4"
+        className="flex flex-col mx-6 md:mx-0 justify-center space-y-4"
         style={{ width: window.innerWidth <= 500 ? "auto" : "512px" }}
       >
-        <h1 className="text-4xl md:text-6xl font-black mb-2">
+        <h1 className="text-3xl md:text-6xl font-black mb-2">
           Recuperar minha senha{" "}
         </h1>
         <form
@@ -87,7 +87,13 @@ function ForgetPassword(): JSX.Element {
             className="bg-indigo-600 hover:bg-indigo-700 text-white text-lg w-full py-3.5 rounded-xl"
             disabled={loading}
           >
-            {loading ? <Spinner /> : "Recuperar [R]"}
+            {loading ? (
+              <Spinner />
+            ) : (
+              <>
+                Recuperar <span className="hidden md:inline">[R]</span>
+              </>
+            )}
           </button>
         </form>
         <div className="text-center pt-4">
@@ -96,11 +102,11 @@ function ForgetPassword(): JSX.Element {
             className="cursor-pointer hover:text-indigo-600 text-indigo-500 font-bold"
             onClick={() => navigate("/sign-in")}
           >
-            Entrar [E]
+            Entrar <span className="hidden md:inline">[E]</span>
           </button>
         </div>
       </div>
-      <div>
+      <div className="hidden md:flex">
         <img src={pluto} style={{ height: "460px" }} alt="" />
       </div>
     </div>
