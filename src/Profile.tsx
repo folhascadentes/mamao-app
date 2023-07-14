b90344ba72c0c4cea1ffa872c06c23e7667ff715
RenanTashiro <renantashiro@hotmail.com>
2023-07-14 11:00:04 -0300

    feat(): feedback loading profile info

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { Skeleton } from "@chakra-ui/react";
 
 function Profile(): JSX.Element {
   const [profile, setProfile] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       try {
         const response = await fetch(
           `${process.env.REACT_APP_BACK_END_API}/profile`,
@@ -25,6 +28,8 @@ function Profile(): JSX.Element {
       } catch (error) {
         localStorage.removeItem("token");
         window.location.href = "/login";
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -37,27 +42,46 @@ function Profile(): JSX.Element {
         <div className="flex flex-col space-y-2 mb-6">
           <h1 className="text-3xl xl:text-4xl mb-4">Meu Perfil</h1>
           <label className="font-black">identificador</label>
-          <p>{profile?.sub}</p>
+          <Skeleton height="24px" rounded={"md"} isLoaded={!isLoading}>
+            {profile?.sub}
+          </Skeleton>
           <label className="font-black">E-mail</label>
-          <p>{profile?.email}</p>
+          <Skeleton height="24px" rounded={"md"} isLoaded={!isLoading}>
+            {profile?.email}
+          </Skeleton>
           <label className="font-black">Etinia</label>
-          <p>{profile?.ethnicity}</p>
+          <Skeleton height="24px" rounded={"md"} isLoaded={!isLoading}>
+            {profile?.ethnicity}
+          </Skeleton>
           <label className="font-black">Deficiência</label>
-          <p>{profile?.deficiency}</p>
+          <Skeleton height="24px" rounded={"md"} isLoaded={!isLoading}>
+            {profile?.deficiency}
+          </Skeleton>
           <label className="font-black">Idade</label>
-          <p>{profile?.age} anos</p>
+          <Skeleton height="24px" rounded={"md"} isLoaded={!isLoading}>
+            {profile?.age} anos
+          </Skeleton>
           <label className="font-black">Altura</label>
-          <p>{profile?.height} cm</p>
+          <Skeleton height="24px" rounded={"md"} isLoaded={!isLoading}>
+            {profile?.height} cm
+          </Skeleton>
           <label className="font-black">Peso</label>
-          <p>{profile?.weight} Kg</p>
+          <Skeleton height="24px" rounded={"md"} isLoaded={!isLoading}>
+            {profile?.weight} Kg
+          </Skeleton>
         </div>
         <div className="flex flex-col space-y-4 mb-6 w-100 md:w-1/3">
           <label className="text-lime-500 text-3xl md:text-6xl font-bold">
             Minhas sinalizações
           </label>
-          <p className="text-6xl md:text-9xl text-orange-500 font-black">
+          <Skeleton
+            height="128px"
+            rounded={"md"}
+            isLoaded={!isLoading}
+            className="text-6xl md:text-9xl text-orange-500 font-black"
+          >
             {(profile?.signs ?? "0")?.toLocaleString("en-DE")}
-          </p>
+          </Skeleton>
           <h2 className="text-xl font-black">Gerenciar conta</h2>
           <button
             className="disabled:bg-opacity-80 bg-red-500 hover:bg-red-600 text-white py-3 xl:py-4 px-6 text-lg xl:text-xl rounded-xl"
@@ -72,7 +96,8 @@ function Profile(): JSX.Element {
           </p>
           <p className="text-sm w-86 font-bold">
             No beta fechado ainda não é possível apagar seus dados pela
-            plataforma. Caso deseje entre em contato com renantashiro@hotmail.com
+            plataforma. Caso deseje entre em contato com
+            renantashiro@hotmail.com
           </p>
         </div>
       </div>
